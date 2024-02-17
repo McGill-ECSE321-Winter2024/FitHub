80710fbec2042f3e8280f07ecf1f33baf1eb93a4
fathisahar <sahar.fathi@mail.mcgill.ca>
2024-02-17 11:50:10 -0500

    deleting image repo

--- sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package ca.mcgill.ecse321.sportcenter.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ca.mcgill.ecse321.sportcenter.model.Image;
-
-public interface ImageRepository extends CrudRepository<Image, Integer> {
-    Image findImageById(int id);
-}
