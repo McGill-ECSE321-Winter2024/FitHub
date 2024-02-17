11b59d509d1bba79d9c99f349350f89c81e7037b
EmiliaGagne <emiliagagne03@gmail.com>
2024-02-17 17:10:26 -0500

    SportCenter tests are working

---
+++ sportcenter/src/test/java/ca/mcgill/ecse321/sportcenter/repository/SportCenterRepositoryTests.java
@@ -0,0 +1,61 @@
+package ca.mcgill.ecse321.sportcenter.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.sql.Date;
+import java.sql.Time;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ca.mcgill.ecse321.sportcenter.model.BillingAccount;
+import ca.mcgill.ecse321.sportcenter.model.SportCenter;
+
+@SpringBootTest
+public class SportCenterRepositoryTests {
+
+    @Autowired
+    private SportCenterRepository sportCenterRepo;
+
+    @BeforeEach
+    @AfterEach
+    public void clearDatabase(){
+        sportCenterRepo.deleteAll();
+    }
+
+    @Test
+    public void testCreateAndReadSportCenter(){
+
+        String name = "FitHub";
+		Time openTime = Time.valueOf("08:00:00");
+        Time closeTime = Time.valueOf("18:00:00");
+		String email = "info@fithub.com";
+		String phone = "421-436-4444";
+		String address = "2011, University Street, Montreal";
+		
+		SportCenter sportsCenter = new SportCenter(name, openTime, closeTime, address, email, phone);
+
+		sportsCenter = sportCenterRepo.save(sportsCenter);
+        int centerId = sportsCenter.getId();
+
+        SportCenter sportCenterFromDb = sportCenterRepo.findSportCenterById(centerId);
+
+        assertNotNull(sportCenterFromDb);
+        assertEquals(name, sportCenterFromDb.getName());
+        assertEquals(openTime.toString(),sportCenterFromDb.getOpeningTime().toString());
+        assertEquals(closeTime.toString(), sportCenterFromDb.getClosingTime().toString());
+        assertEquals(email, sportCenterFromDb.getEmail());
+        assertEquals(phone, sportCenterFromDb.getPhoneNumber());
+        assertEquals(address, sportCenterFromDb.getAddress());
+
+        
+    }
+
+
+    
+}
