9466fcc185d194c37b3c6c4aaf275bc9c6bc0dc2
Anjii00 <anjalisinghal610@gmail.com>
2024-02-17 13:56:23 -0500

    CourseRepo Test update

--- sportcenter/src/test/java/ca/mcgill/ecse321/sportcenter/repository/CourseRepositoryTest.java
+++ sportcenter/src/test/java/ca/mcgill/ecse321/sportcenter/repository/CourseRepositoryTest.java
@@ -9,12 +9,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import ca.mcgill.ecse321.sportcenter.model.Course;
 import ca.mcgill.ecse321.sportcenter.model.Course.Difficulty;
 import ca.mcgill.ecse321.sportcenter.model.Course.Status;
 
 @SpringBootTest
 public class CourseRepositoryTest {
-     @Autowired
+    
+    @Autowired
     private CourseRepository repo;
 
     @BeforeEach
@@ -24,12 +26,27 @@ public class CourseRepositoryTest {
     }
 
     @Test
-    public void testCreateAndReadInEvent() {
+    public void testCreateAndReadCourse() {
         String name = "Cardio";
         String description = "Your instructor will have your heart rate up while you move through a variety of different exercises like running, jump rope, squat jumps, lunges cycling and more.";
         Difficulty diff = Difficulty.Beginner;
         Status status = Status.Approved;
-        //Integer id = 
+
+        Course course = new Course(name, description, diff, status);
+
+        course = repo.save(course);
+        int courseId = course.getId();
+
+        Course courseFromDb = repo.findCourseById(courseId);
+
+        //Assertions
+        assertNotNull(courseFromDb);
+        assertEquals(courseId, courseFromDb.getId());
+        assertEquals(name, courseFromDb.getName());
+        assertEquals(description, courseFromDb.getDescription());
+        assertEquals(diff, courseFromDb.getDifficulty());
+        assertEquals(status, courseFromDb.getName());
+
     }
 
 }
