b1a48bf6cd80ac9193d37c5f22c838b97d67c181
EmiliaGagne <emiliagagne03@gmail.com>
2024-03-03 01:01:03 -0500

    Created Session RestController and added update/delete method to service

--- sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/controller/SessionController.java
+++ sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/controller/SessionController.java
@@ -1,13 +1,46 @@
 package ca.mcgill.ecse321.sportcenter.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import ca.mcgill.ecse321.sportcenter.model.Session;
+import ca.mcgill.ecse321.sportcenter.service.SessionService;
+
 @RestController
 public class SessionController {
+
+    @Autowired
+    private SessionService sessionService;
+
+    @GetMapping("/sessions/{sid}")
+    public Session findSessionById(@PathVariable int sid){
+        return sessionService.findSessionById(sid);
+    }
+
+    @GetMapping("/sessions")
+    public Iterable<Session> findAllSessions(){
+        return sessionService.findAllSessions();
+    }
+
+    @PostMapping("/sessions")
+    public Session proposeSuperviseSession(@RequestBody Session session){
+        return sessionService.proposeSuperviseSession(session.getStartTime(), session.getEndTime(), session.getDate(), session.getCapacity(), session.getSupervisor(), session.getCourseType(), session.getLocation());
+    }
+
+    @PutMapping("/sessions/{id}")
+    public Session updatSession(@RequestBody Session newSession, @PathVariable int id){
+        return sessionService.updateSession(id, newSession.getStartTime(), newSession.getEndTime(), newSession.getDate(), newSession.getCapacity(), newSession.getSupervisor(), newSession.getCourseType(), newSession.getLocation());
+    }
+    
+    @DeleteMapping("/sessions/{id}")
+    public void cancelSession(@PathVariable int id){
+        sessionService.cancelSession(id);
+    }
     
 }
--- sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/service/SessionService.java
+++ sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/service/SessionService.java
@@ -20,7 +20,7 @@ public class SessionService {
     private SessionRepository sessionRepo;
 
     @Transactional
-    public Iterable<Session> findAllSession() {
+    public Iterable<Session> findAllSessions() {
         return sessionRepo.findAll();
     }
 
@@ -29,10 +29,35 @@ public class SessionService {
         return sessionRepo.findSessionById(sid);
     }
  
+    //An instructor propose to supervise a session
     @Transactional
-    public Session createSession(Time aStartTime, Time aEndTime, Date aDate, int aCapacity, Instructor aSupervisor, Course aCourseType, Location aLocation){
+    public Session proposeSuperviseSession(Time aStartTime, Time aEndTime, Date aDate, int aCapacity, Instructor aSupervisor, Course aCourseType, Location aLocation){
         Session sessionToCreate = new Session(aStartTime, aEndTime, aDate, aCapacity, aSupervisor, aCourseType, aLocation);
         return sessionRepo.save(sessionToCreate);
     }
+
+    @Transactional
+    public Session updateSession(int sid, Time aStartTime, Time aEndTime, Date aDate, int aCapacity, Instructor aSupervisor, Course aCourseType, Location aLocation){
+        Session sessionToUpdate = findSessionById(sid);
+        sessionToUpdate.setStartTime(aStartTime);
+        sessionToUpdate.setEndTime(aEndTime);
+        sessionToUpdate.setDate(aDate);
+        sessionToUpdate.setCapacity(aCapacity);
+        sessionToUpdate.setSupervisor(aSupervisor);
+        sessionToUpdate.setCourseType(aCourseType);
+        sessionToUpdate.setLocation(aLocation);
+        return sessionRepo.save(sessionToUpdate);
+    }
+
+    @Transactional
+    //An instructor or an owner cancel a session
+    public void cancelSession(int sid){
+        Session sessionToCancel = sessionRepo.findSessionById(sid);
+        sessionRepo.delete(sessionToCancel);
+        //I am unsure if this line is necessary
+        sessionToCancel.delete();
+
+    }
+
     
 }
