82c78cb7d846be650df5f1cbf2dcbe17bee215ce
James Luu <jamesluu0917@gmail.com>
2024-03-06 00:25:23 -0500

    finished service class and implemented almost all tests. 2 tests left to fix

--- sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/service/SportCenterManagementService.java
+++ sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/service/SportCenterManagementService.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.sportcenter.service;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,13 +9,69 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ca.mcgill.ecse321.sportcenter.repository.SportCenterRepository;
-import ca.mcgill.ecse321.sportcenter.repository.SessionRepository;
-import ca.mcgill.ecse321.sportcenter.model.Location;
-//import ca.mcgill.ecse321.sportcenter.model.SportCenter;;
+import ca.mcgill.ecse321.sportcenter.model.SportCenter;
 
+@Service
 public class SportCenterManagementService {
 
     @Autowired
     private SportCenterRepository sportCenterRepository;
+
+    @Transactional
+	public SportCenter createSportCenter(String name, Time openingTime, Time closingTime, String address, String email, String phoneNumber) {
+		SportCenter sportCenter = new SportCenter();
+		sportCenter.setName(name);
+        sportCenter.setOpeningTime(openingTime);
+        sportCenter.setClosingTime(closingTime);
+        sportCenter.setAddress(address);
+        sportCenter.setEmail(email);
+        sportCenter.setPhoneNumber(phoneNumber);
+		sportCenterRepository.save(sportCenter);
+		return sportCenter;
+	}
+
+    @Transactional
+    public SportCenter findSportCenterById(int id) {
+        SportCenter sportCenter = sportCenterRepository.findSportCenterById(id);
+
+        if (sportCenter == null) {
+            throw new IllegalArgumentException("There is no sport center with ID " + id + ".");
+        }
+
+        return sportCenter;
+    }
+
+    @Transactional
+    public List<SportCenter> getAllSportCenters() {
+        return toList(sportCenterRepository.findAll());
+    }
+
+    @Transactional
+    public SportCenter updateOpeningTime(int id, Time openingTime) {
+        SportCenter sportCenter = findSportCenterById(id);
+
+        sportCenter.setOpeningTime(openingTime);
+        sportCenterRepository.save(sportCenter);
+
+        return sportCenter;
+    }
+
+    @Transactional
+    public SportCenter updateClosingTime(int id, Time closingTime) {
+        SportCenter sportCenter = findSportCenterById(id);
+
+        sportCenter.setClosingTime(closingTime);
+        sportCenterRepository.save(sportCenter);
+
+        return sportCenter;
+    }
+
+    private <T> List<T> toList(Iterable<T> iterable){
+		List<T> resultList = new ArrayList<T>();
+		for (T t : iterable) {
+			resultList.add(t);
+		}
+		return resultList;
+	}
 
 }
---
+++ sportcenter/src/test/java/ca/mcgill/ecse321/sportcenter/service/SportCenterManagementServiceTests.java
@@ -0,0 +1,133 @@
+package ca.mcgill.ecse321.sportcenter.service;
+
+import java.sql.Time;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Answers.valueOf;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import ca.mcgill.ecse321.sportcenter.model.Location;
+import ca.mcgill.ecse321.sportcenter.model.SportCenter;
+import ca.mcgill.ecse321.sportcenter.repository.SportCenterRepository;
+
+@SpringBootTest
+public class SportCenterManagementServiceTests {
+
+    @Mock
+    private SportCenterRepository sportCenterRepository;
+
+    @InjectMocks
+    private SportCenterManagementService sportCenterManagementService;
+
+    private SportCenter newSportCenter(String name, Time openingTime, Time closingTime, String address, String email, String phoneNumber) {
+        SportCenter sportCenter = new SportCenter();
+        sportCenter.setName(name);
+        sportCenter.setOpeningTime(openingTime);
+        sportCenter.setClosingTime(closingTime);
+        sportCenter.setAddress(address);
+        sportCenter.setEmail(email);
+        sportCenter.setPhoneNumber(phoneNumber);
+        return sportCenter;
+    };
+
+    private void checkResultSportCenter(SportCenter sportCenter, String name, Time openingTime, Time closingTime, String address, String email, String phoneNumber) {
+        assertNotNull(sportCenter);
+        assertEquals(name, sportCenter.getName());
+        assertEquals(openingTime, sportCenter.getOpeningTime());
+        assertEquals(closingTime, sportCenter.getClosingTime());
+        assertEquals(address, sportCenter.getAddress());
+        assertEquals(email, sportCenter.getEmail());
+        assertEquals(phoneNumber, sportCenter.getPhoneNumber());
+    }
+
+    @Test
+    public void testCreateValidSportCenter() {
+        String name = "aName";
+        Time openingTime = Time.valueOf("6:0:0");
+        Time closingTime = Time.valueOf("0:0:0");
+        String address = "aAddress";
+        String email = "aEmail";
+        String phoneNumber = "aPhoneNumber";
+
+        SportCenter sportCenter = newSportCenter(name, openingTime, closingTime, address, email, phoneNumber);
+
+        when(sportCenterRepository.save(any(SportCenter.class))).thenReturn(sportCenter);
+
+        SportCenter createdSportCenter = sportCenterManagementService.createSportCenter(name, openingTime, closingTime, address, email, phoneNumber);
+        
+        checkResultSportCenter(createdSportCenter, name, openingTime, closingTime, address, email, phoneNumber);
+        verify(sportCenterRepository, times(1)).save(createdSportCenter);
+    }
+
+    @Test
+    public void testUpdateOpeningTime() {
+        String name = "aName";
+        Time openingTime = Time.valueOf("6:0:0");
+        Time closingTime = Time.valueOf("0:0:0");
+        String address = "aAddress";
+        String email = "aEmail";
+        String phoneNumber = "aPhoneNumber";
+
+        Time newOpeningTime = Time.valueOf("9:0:0");
+
+        SportCenter sportCenter = newSportCenter(name, openingTime, closingTime, address, email, phoneNumber);
+        
+        when(sportCenterRepository.save(any(SportCenter.class))).thenReturn(sportCenter);
+
+        SportCenter updatedSportCenter = sportCenterManagementService.updateOpeningTime(sportCenter.getId(), newOpeningTime);
+        
+        assertEquals(newOpeningTime, updatedSportCenter.getOpeningTime());
+    }
+
+    @Test
+    public void testUpdateClosingTime() {
+        String name = "aName";
+        Time openingTime = Time.valueOf("6:0:0");
+        Time closingTime = Time.valueOf("0:0:0");
+        String address = "aAddress";
+        String email = "aEmail";
+        String phoneNumber = "aPhoneNumber";
+
+        Time newClosingTime = Time.valueOf("22:0:0");
+
+        SportCenter sportCenter = newSportCenter(name, openingTime, closingTime, address, email, phoneNumber);
+        
+        when(sportCenterRepository.save(any(SportCenter.class))).thenReturn(sportCenter);
+
+        SportCenter updatedSportCenter = sportCenterManagementService.updateClosingTime(sportCenter.getId(), newClosingTime);
+        
+        assertEquals(newClosingTime, updatedSportCenter.getClosingTime());
+    }
+
+    @Test
+    public void testReadSportCenterByValidId() {
+        int id = 1;
+        SportCenter sportCenter = newSportCenter("aName", Time.valueOf("6:0:0"), Time.valueOf("0:0:0"), "aAddress", "aEmail", "aPhoneNumber");
+        
+        when(sportCenterRepository.findSportCenterById(id)).thenReturn(sportCenter);
+
+        SportCenter foundSportCenter = sportCenterManagementService.findSportCenterById(id);
+        checkResultSportCenter(foundSportCenter, sportCenter.getName(), sportCenter.getOpeningTime(), sportCenter.getClosingTime(), sportCenter.getAddress(), sportCenter.getEmail(), sportCenter.getPhoneNumber());
+    }
+
+    @Test
+    public void testReadSportCenterByInvalidId() {
+        int id = 1;
+        when(sportCenterRepository.findSportCenterById(id)).thenReturn(null);
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> sportCenterManagementService.findSportCenterById(id));
+        assertEquals("There is no sport center with ID " + id + ".", e.getMessage());
+    }
+
+
+}
