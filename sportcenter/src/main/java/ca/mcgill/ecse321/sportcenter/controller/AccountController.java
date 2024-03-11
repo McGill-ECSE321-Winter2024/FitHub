a534132cb75a18230c0a1e309f595e1ec0c6a5ef
julia-b-grenier <julia.grenier@mail.mcgill.ca>
2024-03-11 19:39:28 -0400

    Fixed issues
    Fixed Mapping mistakes to use PostMapping, PutMapping and DeleteMapping
    
    Also used ResquestParam for the email
    https://www.baeldung.com/spring-requestmapping

--- sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/controller/AccountController.java
+++ sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/controller/AccountController.java
@@ -1,8 +1,14 @@
 package ca.mcgill.ecse321.sportcenter.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.sportcenter.dto.AccountListDTO;
@@ -17,103 +23,106 @@ public class AccountController {
     @Autowired
     AccountService accountService;
     
-    @GetMapping("/customers")
+    @PostMapping(value={"/customers", "/customers/"})
+    @ResponseStatus(HttpStatus.CREATED)
     public AccountResponseDTO createCustomerAccount(@RequestBody AccountRequestDTO account) {
         Account createdAccount = (Account) accountService.createCustomerAccount(account.getEmail(), account.getPassword(), account.getName(), account.getImageURL());
         return new AccountResponseDTO(createdAccount);
     }
     
-    @GetMapping("/instructors")
+    @PostMapping(value={"/instructors", "/instructors/"})
+    @ResponseStatus(HttpStatus.CREATED)
     public AccountResponseDTO createInstructorAccount(@RequestBody AccountRequestDTO account) {
         Account createdAccount = (Account) accountService.createInstructorAccount(account.getEmail(), account.getPassword(), account.getName(), account.getImageURL());
         return new AccountResponseDTO(createdAccount);
     }
     
-    @GetMapping("/owners")
+    @PostMapping(value={"/owners", "/owners/"})
+    @ResponseStatus(HttpStatus.CREATED)
     public AccountResponseDTO createOwnerAccount(@RequestBody AccountRequestDTO account) {
         Account createdAccount = (Account) accountService.createOwnerAccount(account.getEmail(), account.getPassword(), account.getName(), account.getImageURL());
         return new AccountResponseDTO(createdAccount);
     }
 
-    @GetMapping("/customers/{id}")
-    public AccountResponseDTO updateCustomerAccount(@RequestBody Integer id, @RequestBody AccountRequestDTO account) {
+    @PutMapping(value={"/customers/{id}", "/customers/{id}/"})
+    public AccountResponseDTO updateCustomerAccount(@PathVariable Integer id, @RequestBody AccountRequestDTO account) {
         Account updatedAccount = (Account) accountService.updateCustomerAccount(id, account.getEmail(), account.getPassword(), account.getName(), account.getImageURL());
         return new AccountResponseDTO(updatedAccount);
     }
     
-    @GetMapping("/instructors/{id}")
-    public AccountResponseDTO updateInstructorAccount(@RequestBody Integer id, @RequestBody AccountRequestDTO account) {
+    @PutMapping(value={"/instructors/{id}", "/instructors/{id}/"})
+    public AccountResponseDTO updateInstructorAccount(@PathVariable Integer id, @RequestBody AccountRequestDTO account) {
         Account updatedAccount = (Account) accountService.updateInstructorAccount(id, account.getEmail(), account.getPassword(), account.getName(), account.getImageURL());
         return new AccountResponseDTO(updatedAccount);
     }
     
-    @GetMapping("/owners/{id}")
-    public AccountResponseDTO updateOwnerAccount(@RequestBody Integer id, @RequestBody AccountRequestDTO account) {
+    @PutMapping(value={"/owners/{id}", "/owners/{id}/"})
+    public AccountResponseDTO updateOwnerAccount(@PathVariable Integer id, @RequestBody AccountRequestDTO account) {
         Account updatedAccount = (Account) accountService.updateOwnerAccount(id, account.getEmail(), account.getPassword(), account.getName(), account.getImageURL());
         return new AccountResponseDTO(updatedAccount);
     }
 
-    @GetMapping("/customers/{id}")
-    public void deleteCustomerAccount(@RequestBody Integer id) {
+    @DeleteMapping(value={"/customers/{id}", "/customers/{id}/"})
+    public void deleteCustomerAccount(@PathVariable Integer id) {
         accountService.deleteCustomerAccount(id);
     }
     
-    @GetMapping("/instructors/{id}")
-    public void deleteInstructorAccount(@RequestBody Integer id) {
+    @DeleteMapping(value={"/instructors/{id}", "/instructors/{id}/"})
+    public void deleteInstructorAccount(@PathVariable Integer id) {
         accountService.deleteInstructorAccount(id);
     }
     
-    @GetMapping("/owners/{id}")
-    public void deleteOwnerAccount(@RequestBody Integer id, @RequestBody AccountRequestDTO account) {
+    @DeleteMapping(value={"/owners/{id}", "/owners/{id}/"})
+    public void deleteOwnerAccount(@PathVariable Integer id, @RequestBody AccountRequestDTO account) {
         accountService.deleteOwnerAccount(id);
     }
     
-    @GetMapping("/customers/{id}")
+    @GetMapping(value={"/customers/{id}", "/customers/{id}/"})
     public AccountResponseDTO findCustomerById(@PathVariable Integer id) {
         return new AccountResponseDTO((Account) accountService.findCustomerById(id));
     }
     
-    @GetMapping("/instructors/{id}")
+    @GetMapping(value={"/instructors/{id}", "/instructors/{id}/"})
     public AccountResponseDTO findInstructorById(@PathVariable Integer id) {
         return new AccountResponseDTO((Account) accountService.findInstructorById(id));
     }
     
-    @GetMapping("/owners/{id}")
+    @GetMapping(value={"/owners/{id}", "/owners/{id}/"})
     public AccountResponseDTO findOwnerById(@PathVariable Integer id) {
         return new AccountResponseDTO((Account) accountService.findOwnerById(id));
     }
     
-    @GetMapping("/customers/{email}")
-    public AccountResponseDTO findCustomerByEmail(@PathVariable String email) {
+    @GetMapping(value={"/customers", "/customers/"})
+    public AccountResponseDTO findCustomerByEmail(@RequestParam("email") String email) {
         return new AccountResponseDTO((Account) accountService.findCustomerByEmail(email));
     }
     
-    @GetMapping("/instructors/{email}")
-    public AccountResponseDTO findInstructorByEmail(@PathVariable String email) {
+    @GetMapping(value={"/instructors", "/instructors/"})
+    public AccountResponseDTO findInstructorByEmail(@RequestParam("email") String email) {
         return new AccountResponseDTO((Account) accountService.findInstructorByEmail(email));
     }
     
-    @GetMapping("/owners/{email}")
-    public AccountResponseDTO findOwnerByEmail(@PathVariable String email) {
+    @GetMapping(value={"/owners", "/owners/"})
+    public AccountResponseDTO findOwnerByEmail(@RequestParam("email") String email) {
         return new AccountResponseDTO((Account) accountService.findOwnerByEmail(email));
     }
 
-    @GetMapping("/customers")
+    @GetMapping(value={"/customers", "/customers/"})
     public AccountListDTO findAllCustomers() {
         return new AccountListDTO(AccountListDTO.accountListToAccountResponseDTOList(accountService.findAllCustomers()));
     }
 
-    @GetMapping("/instructors")
+    @GetMapping(value={"/instructors", "/instructors/"})
     public AccountListDTO findAllInstructors() {
         return new AccountListDTO(AccountListDTO.accountListToAccountResponseDTOList(accountService.findAllInstructors()));
     }
 
-    @GetMapping("/owners")
+    @GetMapping(value={"/owners", "/owners/"})
     public AccountListDTO findAllOwners() {
         return new AccountListDTO(AccountListDTO.accountListToAccountResponseDTOList(accountService.findAllOwners()));
     }
 
-    @GetMapping("/accounts")
+    @GetMapping(value={"/accounts", "/accounts/"})
     public AccountListDTO findAllAccounts() {
         return new AccountListDTO(AccountListDTO.accountListToAccountResponseDTOList(accountService.findAllAccounts()));
     }
