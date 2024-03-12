cb4bdae3f9132ec159cd00777280b42d76a78ebc
Tayba <tayba.jusab25@gmail.com>
2024-03-12 12:21:28 -0400

    Modify AccountResponseDTO to take into account Customer, Instructor and Owner ResponseDTO

---
+++ sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/dto/RegistrationResponseDTO.java
@@ -0,0 +1,31 @@
+package ca.mcgill.ecse321.sportcenter.dto;
+
+import ca.mcgill.ecse321.sportcenter.model.Registration;
+
+public class RegistrationResponseDTO {
+    private AccountResponseDTO account;
+    private SessionResponseDTO session;
+
+    public RegistrationResponseDTO(Registration registration) {
+        this.account = AccountResponseDTO.create(registration.getKey().getAccount());
+        this.session = new SessionResponseDTO(registration.getKey().getSession());
+    }
+
+    public AccountResponseDTO getAccount() {
+        return this.account;
+    }
+
+    public void setAccount(AccountResponseDTO account) {
+        this.account = account;
+    }
+
+    public SessionResponseDTO getSession() {
+        return this.session;
+    }
+
+    public void setSession(SessionResponseDTO session) {
+        this.session = session;
+    }
+
+
+}
