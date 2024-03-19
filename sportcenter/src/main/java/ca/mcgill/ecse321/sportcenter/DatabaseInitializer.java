ce1ebda0c9d7abcb7edebcc302c9014493626a2e
julia-b-grenier <julia.grenier@mail.mcgill.ca>
2024-03-19 11:54:44 -0400

    Fixing mistakes in previous commit
    Did not consider that we also have to create a unique sportCenter and that creating a owner requires a sportCenter

--- sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/DatabaseInitializer.java
+++ sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/DatabaseInitializer.java
@@ -1,15 +1,21 @@
 package ca.mcgill.ecse321.sportcenter;
 
+import java.sql.Time;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import ca.mcgill.ecse321.sportcenter.service.AccountService;
+import ca.mcgill.ecse321.sportcenter.service.SportCenterManagementService;
 
 @Component
 public class DatabaseInitializer implements CommandLineRunner {
     @Autowired
     private AccountService accountService;
+    
+    @Autowired
+    private SportCenterManagementService sportCenterService;
 
     @Override
     public void run(String... args) throws Exception {
@@ -17,7 +23,14 @@ public class DatabaseInitializer implements CommandLineRunner {
     }
 
     private void initializeDatabase() {
-        // Create a default if it isnt already in the database
+        // If the unique sportCenter is not there, create it
+        if (sportCenterService.getAllSportCenters().isEmpty()) {
+            Time openingTime = Time.valueOf("6:0:0");
+            Time closingTime = Time.valueOf("23:59:0");
+            sportCenterService.createSportCenter("Fithub", openingTime, closingTime, "16", "sportcenter@mail.com", "455-645-4566");
+        }
+
+        // Create a default owner Admin account if it isnt already in the database
         try {
             accountService.findOwnerByEmail("@"); // If it throws an error here then create one
             System.out.println("Default account @ is already created");
