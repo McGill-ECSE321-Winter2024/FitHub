56f3c8366ee77cf4e1d985bad6188340a73c9f95
Anjii00 <anjalisinghal610@gmail.com>
2024-03-23 12:44:49 -0400

    changes

---
+++ sportcenter/src/test/java/ca/mcgill/ecse321/sportcenter/integration/BillingAccountIntegreationTests.java
@@ -0,0 +1,42 @@
+package ca.mcgill.ecse321.sportcenter.integration;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.sql.Time;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.event.annotation.AfterTestClass;
+
+import ca.mcgill.ecse321.sportcenter.service.BillingAccountService;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(Lifecycle.PER_CLASS)
+public class BillingAccountIntegreationTests {
+
+    @Autowired
+    private TestRestTemplate client;
+
+    @Autowired
+    BillingAccountService billingService;
+    
+}
