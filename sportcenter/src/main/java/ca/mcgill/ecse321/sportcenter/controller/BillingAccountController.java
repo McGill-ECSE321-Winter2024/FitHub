7b6331c5be96500af0c587c6fd7dc5894d8ee387
Anjii00 <anjalisinghal610@gmail.com>
2024-03-23 15:03:22 -0400

    testing

--- sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/controller/BillingAccountController.java
+++ sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/controller/BillingAccountController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,7 @@ import ca.mcgill.ecse321.sportcenter.model.Customer;
 import ca.mcgill.ecse321.sportcenter.service.AccountService;
 import ca.mcgill.ecse321.sportcenter.service.BillingAccountService;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 public class BillingAccountController {
 
--- sportcenter/src/test/java/ca/mcgill/ecse321/sportcenter/integration/BillingAccountIntegreationTests.java
+++ sportcenter/src/test/java/ca/mcgill/ecse321/sportcenter/integration/BillingAccountIntegreationTests.java
@@ -4,6 +4,10 @@ package ca.mcgill.ecse321.sportcenter.integration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.description;
+
+import java.math.BigInteger;
+import java.sql.Date;
 import java.sql.Time;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -25,7 +29,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.event.annotation.AfterTestClass;
 
+import ca.mcgill.ecse321.sportcenter.dto.BillingAccountListDTO;
+import ca.mcgill.ecse321.sportcenter.dto.LoginRequestDTO;
+import ca.mcgill.ecse321.sportcenter.dto.LoginResponseDTO;
+import ca.mcgill.ecse321.sportcenter.dto.SessionListDTO;
+import ca.mcgill.ecse321.sportcenter.model.Customer;
+import ca.mcgill.ecse321.sportcenter.model.Instructor;
+import ca.mcgill.ecse321.sportcenter.repository.BillingAccountRepository;
+import ca.mcgill.ecse321.sportcenter.repository.CustomerRepository;
+import ca.mcgill.ecse321.sportcenter.repository.SportCenterRepository;
+import ca.mcgill.ecse321.sportcenter.service.AccountService;
 import ca.mcgill.ecse321.sportcenter.service.BillingAccountService;
+import ca.mcgill.ecse321.sportcenter.service.SportCenterManagementService;
 
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -37,6 +52,134 @@ public class BillingAccountIntegreationTests {
     private TestRestTemplate client;
 
     @Autowired
-    BillingAccountService billingService;
+    private BillingAccountService billingService;
+
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    SportCenterManagementService sportCenterService;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private BillingAccountRepository billingAccountRepository;
+
+    @Autowired
+    private SportCenterRepository sportCenterRepository;
+
+    //---------------------Headers------------------------------
+
+	private String LOGIN_EMAIL = "julia@mail.com";
+    private String LOGIN_PASSWORD = "secret1456165";
+
+	//------------------------- Customer ----------------------
+
+	String email = "olivia@mail.com";
+    String password = "secretPassword";
+    String instructorName = "Olivia";
+    String imageURL = "pfp.png";
+	Customer customer;
+
+	String newEmail = "dada@mail.com";
+    String newPassword = "notsosecretPassword";
+    String newInstructorName = "Dada";
+    String newImageURL = "dada.png";
+	Customer newCustomer;
     
+    //------------------------ BillingAccount -------------------------
+
+    String cardHolder = "Mary Jane";
+    String billingAddress = "1234, Sherbrooke Street, Montreal";
+    BigInteger cardNumber =  new BigInteger("1234567891234567");
+    Integer cvv = 372;
+    boolean isDefault = true;
+    Date expirationDate = Date.valueOf("2026-02-18");
+    int validId = 0;
+
+    String newCardHolder = "Bob Smith";
+    String newBillingAddress = "9, Wellington Street, Montreal";
+    BigInteger newCardNumber =  new BigInteger("2000007891234000");
+    Integer newCvv = 407;
+    boolean newIsDefault = false;
+    Date newExpirationDate = Date.valueOf("2028-11-01");
+
+    @BeforeAll
+	public void intializeDatabase() {
+        
+        billingAccountRepository.deleteAll();
+		sportCenterRepository.deleteAll();
+		customerRepository.deleteAll();
+
+        sportCenterService.createSportCenter("Fithub", Time.valueOf("6:0:0"), Time.valueOf("23:0:0"), "16", "sportcenter@mail.com", "455-645-4566");
+		customer = accountService.createCustomerAccount(email, password, instructorName, imageURL);
+		newCustomer = accountService.createCustomerAccount(newEmail, newPassword, newInstructorName, newImageURL);
+
+    }
+
+    @AfterTestClass
+	public void clearDatabase() {
+        billingAccountRepository.deleteAll();
+		sportCenterRepository.deleteAll();
+		customerRepository.deleteAll();
+    }
+
+    //---------------login -------------------------------
+
+    @Test
+    @Order(0)
+    public void login() {
+        // Save one account in the system
+        Time openingTime = Time.valueOf("6:0:0");
+        Time closingTime = Time.valueOf("23:59:0");
+        sportCenterService.createSportCenter("Fithub", openingTime, closingTime, "16", "sportcenter@mail.com", "455-645-4566");
+        
+        accountService.createCustomerAccount(LOGIN_EMAIL, LOGIN_PASSWORD, "Julia", "Doritos.png");
+        
+        // Login into that account
+        LoginRequestDTO request = new LoginRequestDTO(LOGIN_EMAIL, LOGIN_PASSWORD);
+        ResponseEntity<LoginResponseDTO> response = client.postForEntity("/login", request, LoginResponseDTO.class);
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.FOUND, response.getStatusCode());
+    }
+
+    //--------------------------// General Empty Result Tests //--------------------------//
+
+    @Test
+	@Order(1)
+	public void testFindAllBillingAccountEmptyResult(){
+
+		HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth(LOGIN_EMAIL, LOGIN_PASSWORD);
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+		
+		ResponseEntity<BillingAccountListDTO> response = client.exchange("/billing-accounts", HttpMethod.GET, requestEntity, BillingAccountListDTO.class);
+        // Assert
+        assertNotNull(response);
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode()); // Should be empty
+
+	}
+
+    @Test
+	@Order(2)
+	public void testFindBillingAccountByCustomerEmptyResult(){
+
+		HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth(LOGIN_EMAIL, LOGIN_PASSWORD);
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+
+		assertNotNull(customerRepository.findCustomerById(customer.getId()));
+		
+		String url = "/customers/" + customer.getId() + "billing-accounts";
+
+		ResponseEntity<BillingAccountListDTO> response = client.exchange(url, HttpMethod.GET, requestEntity, BillingAccountListDTO.class);
+        // Assert
+        assertNotNull(response);
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode()); // Should be empty
+
+	}
+
+
 }
