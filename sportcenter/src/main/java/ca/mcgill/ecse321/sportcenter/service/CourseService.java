f1fad0c6e0fb954361487a145360d15a1b7ae110
fathisahar <sahar.fathi@mail.mcgill.ca>
2024-03-24 17:43:31 -0400

    removed a bunch of unused imports

--- sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/service/CourseService.java
+++ sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/service/CourseService.java
@@ -4,13 +4,10 @@ import java.util.List;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.method.P;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ca.mcgill.ecse321.sportcenter.model.Course;
-import ca.mcgill.ecse321.sportcenter.model.Course.Difficulty;
-import ca.mcgill.ecse321.sportcenter.model.Course.Status;
 import ca.mcgill.ecse321.sportcenter.repository.CourseRepository;
 
 /*
--- sportcenter/src/test/java/ca/mcgill/ecse321/sportcenter/integration/AccountIntegrationTests.java
+++ sportcenter/src/test/java/ca/mcgill/ecse321/sportcenter/integration/AccountIntegrationTests.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -31,10 +30,6 @@ import ca.mcgill.ecse321.sportcenter.dto.InstructorResponseDTO;
 import ca.mcgill.ecse321.sportcenter.dto.LoginRequestDTO;
 import ca.mcgill.ecse321.sportcenter.dto.LoginResponseDTO;
 import ca.mcgill.ecse321.sportcenter.dto.OwnerResponseDTO;
-import ca.mcgill.ecse321.sportcenter.repository.CustomerRepository;
-import ca.mcgill.ecse321.sportcenter.repository.InstructorRepository;
-import ca.mcgill.ecse321.sportcenter.repository.OwnerRepository;
-import ca.mcgill.ecse321.sportcenter.repository.SportCenterRepository;
 import ca.mcgill.ecse321.sportcenter.service.AccountService;
 import ca.mcgill.ecse321.sportcenter.service.SportCenterManagementService;
 
@@ -52,15 +47,6 @@ public class AccountIntegrationTests extends CommonTestSetup {
 
     @Autowired
     SportCenterManagementService sportCenterService;
-    @Autowired
-    private SportCenterRepository sportCenterRepository;
-
-    @Autowired
-    private CustomerRepository customerRepository;
-    @Autowired
-    private InstructorRepository instructorRepository;
-    @Autowired
-    private OwnerRepository ownerRepository;
 
 
     private String LOGIN_EMAIL = "julia@mail.com";
