d60ddcc606a6535be0f84387d91e683f7befb6d1
Anjii00 <anjalisinghal610@gmail.com>
2024-03-24 13:11:47 -0400

    cross origin added to all controllers

--- sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/controller/CourseController.java
+++ sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/controller/CourseController.java
@@ -3,6 +3,7 @@ package ca.mcgill.ecse321.sportcenter.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import ca.mcgill.ecse321.sportcenter.dto.CourseResponseDTO;
 import ca.mcgill.ecse321.sportcenter.model.Course;
 import ca.mcgill.ecse321.sportcenter.service.CourseService;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class CourseController {
     @Autowired
