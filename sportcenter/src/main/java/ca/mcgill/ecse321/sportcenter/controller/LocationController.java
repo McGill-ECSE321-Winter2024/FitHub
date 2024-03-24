7fc30095f47f6e69488071c9f40d830ebf51b8af
Anjii00 <anjalisinghal610@gmail.com>
2024-03-24 15:54:16 -0400

    small fix again

--- sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/controller/LocationController.java
+++ sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/controller/LocationController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,6 +25,7 @@ import ca.mcgill.ecse321.sportcenter.dto.RegistrationListDTO;
 import ca.mcgill.ecse321.sportcenter.dto.LocationListDTO;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class LocationController {
 
