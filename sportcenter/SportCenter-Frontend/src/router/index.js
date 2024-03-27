61060ac4c824b25e596d08b19caaf7225ef446dc
julia-b-grenier <julia.grenier@mail.mcgill.ca>
2024-03-26 21:26:01 -0400

    Start Login page Rename Folders

--- sportcenter/SportCenter-Frontend/src/router/index.js
+++ sportcenter/SportCenter-Frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import SignIn from '@/components/SignIn'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/login',
+      name: 'SportCenter Login page',
+      component: SignIn
     }
   ]
 })
