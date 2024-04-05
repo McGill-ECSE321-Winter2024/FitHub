4eccd1fd9474056da79062595f47065be338dd8f
julia-b-grenier <julia.grenier@mail.mcgill.ca>
2024-04-05 10:03:57 -0400

    Changing that back

--- SportCenter-Frontend/src/App.vue
+++ SportCenter-Frontend/src/App.vue
@@ -12,12 +12,10 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Figtree', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  
-  overflow: auto;
 }
 </style>
