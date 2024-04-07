4aa18ee8e2efcc74eba2939a23aceb4e2967c94a
fathisahar <sahar.fathi@mail.mcgill.ca>
2024-04-07 14:49:46 -0400

    createcourse changes

--- SportCenter-Frontend/src/components/CreateCourse.vue
+++ SportCenter-Frontend/src/components/CreateCourse.vue
@@ -1,56 +1,63 @@
 <template>
-  <div class="solid-background">
-    <div>
-      <h1 class="custom-h1">Propose a new course</h1>
-      <h3 class="custom-h3">Input details for this course request to the owner</h3>
-      <!-- Container for sidebar and main content -->
-      <div class="content-container">
-        <!-- Properties and text fields -->
-        <div class="text-fields">
-          <div class="text-field-item">
-            <label>Image</label>
-            <input type="url" v-model="course.image" class="text-field" @change="loadImage" />
-            <img v-if="course.image" :src="course.image" alt="Course Image" style="max-width: 100%; margin-top: 10px;" />
-          </div>
-          <div class="text-field-item">
-            <label>Name</label>
-            <input type="name" v-model="course.name" class="text-field" />
-          </div>
-          <div class="text-field-item">
-            <label>Category</label>
-            <input type="category" v-model="course.category" class="text-field" />
-          </div>
-          <div class="text-field-item">
-            <label>Description</label>
-            <textarea v-model="course.description" class="text-field-description" ></textarea>
-          </div>
-          <div class="text-field-item">
-            <label>Difficulty</label>
+
+<div class="solid-background">
+<div>
+<h3 class="custom-h3">Input details for this course request to the owner</h3>
+</div>
+<div class="form-box">
+    
+    <form @submit.prevent="submitForm">
+    <div class="form-group">
+        <label>Image</label>     
+        <input type="text" id="url" v-model="course.url" required>
+      </div>
+      <div class="form-group">
+        <label>Name</label>     
+        <input type="text" id="courseName" v-model="course.name" required>
+      </div>
+      <form-row>
+      <div class="form-group">
+        <label>Category</label>
+        <input type="text" id="category" v-model="course.category" required>
+      </div>
+      <div class="form-group">
+        <label>Description</label>
+        <textarea v-model="course.description" class="text-field-description" ></textarea>
+      </div>
+      </form-row>
+
+      <div class="form-group">
+        <label>Difficulty</label>
             <select v-model="course.difficulty" class="text-field">
               <option value="Beginner">Beginner</option>
               <option value="Intermediate">Intermediate</option>
               <option value="Advanced">Advanced</option>
             </select>
-          </div>
-        </div>
-        <div class="buttons">
-          <button @click="create(course)" class="approve">Create</button>
-          <button @click="cancel()" class="disapprove">Cancel</button>
-        </div>
       </div>
-    </div>
-  </div>
+
+      <button id="save-btn" type="save">Save</button>
+      <button id="cancel-btn" type="cancel">Cancel</button>
+    </form>
+
+</div>
+<p v-if="successMessage">{{ successMessage }}</p>
+</div>
 </template>
 
 <script>
 
 export default {
-  name: "ProfileSettings",
-  components: {
-    // Add more components as needed
-  },
   data() {
     return {
+      formData: {
+        cardNumber: '',
+        expiryDate: '',
+        cvv: '',
+        billingAddress: '',
+        cardHolder: '',
+        isDefault: false,
+        successMessage: '' 
+      },
       course: {
         name: "",
         category: "",
@@ -61,126 +68,193 @@ export default {
     };
   },
   methods: {
-    create(course) {
-      // Implement create method
+      // Here you can handle form submission, for example, sending data to a server
+      //this.successMessage = 'Added card successfully';
+      //console.log('Form submitted:', this.formData);
+    submitForm() {
+      fetch('http://127.0.0.1:8080/customers/{cId}/billing-accounts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(this.formData),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          this.successMessage = data.message; // Assuming your backend returns a success message
+          console.log('Form submitted:', data);
+        })
+        .catch((error) => {
+          console.error('Error submitting form:', error);
+          // Handle error
+        });
+
     },
-    cancel() {
-      // Implement cancel method
+    cancelForm() {
+
+      return{
+        formData: {
+        cardNumber: '',
+        expiryDate: '',
+        cvv: '',
+        billingAddress: '',
+        cardHolder: '',
+        isDefault: false,
+        successMessage: '' 
+        }
+      };
     },
-    loadImage(event) {
-      const input = event.target;
-      if (input.files && input.files[0]) {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-          this.course.image = e.target.result;
-        };
-        reader.readAsDataURL(input.files[0]);
-      }
-    }
   }
 };
 </script>
 
 <style scoped>
 .solid-background {
-  font-family: Figtree;
+  background-color: var(--color-black);
   height: 100vh;
   width: 100vw;
-  background-color: #121212;
-}
-
-.content-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: left;
-  align-items: center;
-  width: 100%;
-}
-
-.text-fields {
-  color: #ffffff;
-  margin-top: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: left;
-}
-
-.text-field-item {
-  margin-bottom: 10px;
-  height: 1000%;
-}
-
-.text-field {
-  width: 100%;
-  padding: 10px;
-  border-radius: 5px;
-  border: 1px solid #444;
-  background-color: transparent;
-  color: #ffffff;
-  box-sizing: border-box;
-  margin-top: 4px;
-  transition: border-color 0.1s ease-in-out, font-weight 0.1s ease-in-out, border-width 0.1s ease-in-out;
-  font-weight: normal;
-  border-width: 1px;
-}
-
-.text-field-description {
-    width: 100%;
-  padding: 10px;
-  border-radius: 5px;
-  border: 1px solid #444;
-  background-color: transparent;
-  color: #ffffff;
-  box-sizing: border-box;
-  margin-top: 4px;
-  transition: border-color 0.1s ease-in-out, font-weight 0.1s ease-in-out, border-width 0.1s ease-in-out;
-  font-weight: normal;
-  border-width: 1px;
-}
-
-textarea.text-field {
-  height: auto; /* Remove fixed height for textarea */
-}
-
-.custom-h3 {
-  font-size: 24px;
-  font-weight: 700;
-  color: #FFFFFF;
-  text-align: left;
-  margin-left: 60px;
+  overflow: auto;
 }
 
 .custom-h1 {
-  color: #ffffff;
+  color: var(--color-white);
   font-size: 55px;
-  text-align: left;
-  margin-left: 60px;
 }
 
-.approve {
-  border: 0px;
-  background-color: #CDF563;
-  color: #121212;
-  font-weight: bold;
-  border-radius: 20px;
-  height: 40px;
-  width: 100px;
+.custom-h3 {
+    margin-top : 50px;
+  font-size: 24px;
+  font-weight: 700;
+  color: var(--color-white);
 }
 
-.disapprove {
-  margin-left: 10px;
-  border: 0px;
-  background-color: #E3240C;
-  color: #121212;
-  font-weight: bold;
-  border-radius: 20px;
-  height: 40px;
-  width: 100px;
+.content {
+  text-align: center;
 }
 
-.buttons {
+.text-field-description,
+.text-field,
+input[type="text"],
+textarea {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 7px;
+  background-color: var(--color-black);
+  color: #ccc;
+  font-family: inherit; /* Inherit font family */
+}
+
+.text-field option {
+  background-color: var(--color-black);
+  color: #ccc;
+}
+
+/* Disable default dropdown arrow */
+.text-field::-ms-expand {
+  display: none;
+}
+
+.text-field::-webkit-select-arrow {
+  display: none;
+}
+
+.text-field::-webkit-scrollbar {
+  width: 8px;
+}
+
+.text-field::-webkit-scrollbar-track {
+  background: var(--color-black);
+}
+
+.text-field::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: 4px;
+}
+
+.text-field:focus,
+input[type="text"]:focus,
+textarea:focus {
+  outline: none;
+  border-color: #fff;
+}
+
+.form-box {
+  width: 600px; /* Adjust the width as needed */
+  margin-top: 100px;
+  margin: auto;
+  padding: 20px;
+  border: 2px solid #ccc;
+  border-radius: 10px;
+}
+
+.form-group-side {
+  margin-bottom: 15px;
   display: flex;
-  justify-content: center;
-  margin-top: 20px;
+  align-items: center;
 }
-</style>
+
+.form-row {
+  display: flex;
+  justify-content: space-between;
+}
+
+.form-group {
+  margin-bottom: 15px;
+}
+
+input[type="checkbox"] {
+  align-self: left;
+  align-items: left;
+  margin-right: 5px;
+}
+
+label {
+  text-align: left;
+  display: block;
+  margin-bottom: 5px;
+  color: #ffffff;
+  font-size: 18px;
+  font-weight: 500;
+}
+
+button {
+  padding: 10px 10px;
+  color: var(--color-black);
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  font-size: 18px;
+  font-weight: 500;
+  width: 100px; 
+  height: 50px; 
+  margin-bottom: 15px;
+}
+
+#save-btn {
+  background-color: #CDF563;
+}
+
+#cancel-btn {
+  margin-left: 75px; 
+  background-color: #EC5545;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+
+h3 {
+  margin-bottom: 50px;
+  font-size: 24px;
+  font-weight: 700;
+  color: #FFFFFF;
+}
+
+
+</style>c
